217c37735e6332536e32dc05626b9b2d47ee0235
anadevti <anacarolayne777@gmail.com>
2024-07-09 17:48:48 -0300

    subindo exercicios 7 e 9 finalizado!

--- Python_Básico/exercicicios/9.py
+++ Python_Básico/exercicicios/9.py
@@ -3,3 +3,10 @@ Mensagem Amigável: Faça um programa que utilize 4 variáveis como preferir e n
 
 Exemplo de retorno: "Olá Maria, prazer te conhecer. Sou de São Paulo também e estou migrando de área." (Lembrando que para o retorno vamos usar print com as variáveis criadas e este texto é somente um exemplo, utilize a criatividade).
 '''
+
+nome = str(input('Qual o seu nome? ')).capitalize()
+idade = int(input('Qual a sua idade? '))
+lugar = str(input('Qual o seu lugar favorito? ')).capitalize()
+profissao = str(input('Qual sua profissão?')).capitalize()
+
+print(f'Olá {nome}, prazer lhe conhecer! sua idade é {idade} anos! vi aqui que o seu lugar favorito é {lugar} e sua profissão é {profissao}! ')
