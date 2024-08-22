d7b15fb593fbef240e4219769f030d860303812e
anadevti <anacarolayne777@gmail.com>
2024-08-22 10:37:12 -0300

    estruturando projeto com render template e bootstrap

--- APIs em Python/Flask-App/app.py
+++ APIs em Python/Flask-App/app.py
@@ -1,7 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
+import urllib.request, json
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return '<h1>Olá Womakers!</h1>'
+def get_list_characters_page():
+    url = "https://rickandmortyapi.com/api/character"
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    dict = json.loads(data)
+
+    return render_template('characters.html', characters=dict['results'])
+@app.route('/lista')
+def get_list_elements():
+    url = "https://rickandmortyapi.com/api/character"
+    response = urllib.request.urlopen(url)
+    characters = response.read()
+    dict = json.loads(characters)
+
+    characters_list = []
+
+    for character in dict['results']:
+        characters = {
+            'name': character['name'],
+            'status': character['status'],
+        }
+        characters_list.append(characters)
+
+    return {'characters': characters_list}
