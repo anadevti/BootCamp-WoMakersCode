24cf1361f3d1b050a620f245a1022c9bc3c03ad7
anadevti <anacarolayne777@gmail.com>
2024-08-28 10:12:22 -0300

    subindo api com alguns bugs que vou corrigir mais tarde

--- APIs em Python/Flask-App/app.py
+++ APIs em Python/Flask-App/app.py
@@ -1,31 +1,45 @@
-from flask import Flask, render_template
-import urllib.request, json
+import requests
+from flask import Flask, render_template, json
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def get_list_characters_page():
     url = "https://rickandmortyapi.com/api/character"
-    response = urllib.request.urlopen(url)
-    data = response.read()
-    dict = json.loads(data)
+    response = requests.get(url)
+    data = response.json()
+    return render_template('characters.html', characters=data['results'])
 
-    return render_template('characters.html', characters=dict['results'])
+@app.route('/test')
+def test_layout():
+    return render_template('profile.html')
+
+
+
+@app.route('/profile/<id>')
+def get_profile(id):
+    url = f"https://rickandmortyapi.com/api/character/{id}"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        character_data = response.json()
+        return render_template('profile.html', profile=character_data)
+    except requests.RequestException as e:
+        return f"An error occurred: {e}", 500
+
 @app.route('/lista')
 def get_list_elements():
     url = "https://rickandmortyapi.com/api/character"
-    response = urllib.request.urlopen(url)
-    characters = response.read()
-    dict = json.loads(characters)
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
 
-    characters_list = []
+        characters_list = [
+            {'name': character['name'], 'status': character['status']}
+            for character in data['results']
+        ]
 
-    for character in dict['results']:
-        characters = {
-            'name': character['name'],
-            'status': character['status'],
-        }
-        characters_list.append(characters)
-
-    return {'characters': characters_list}
+        return {'characters': characters_list}
+    except requests.RequestException as e:
+        return f"An error occurred: {e}", 500
